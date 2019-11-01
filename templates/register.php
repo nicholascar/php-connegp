85c910417faba8765f11256f2ca6fb532d7ad1d2
Nicholas Car <nicholas.car@csiro.au>
2019-11-01 12:13:45 +1000

    fixed link to connegp page

--- templates/register.php
+++ templates/register.php
@@ -2,7 +2,7 @@
     <h4>Alternate Representations</h4>
     <p>You can view this register's content in different formats and according to different profiles (data specifications).</p>
     <p>See the specification of <a href="https://w3c.github.io/dxwg/conneg-by-ap/">Content Negotiation by Profile</a> for how to do this in general.</p>
-    <p>See this register's <a href="http://linked.data.gov.au/org/connegp.php">Conneg P Description page</a> for specifics.</p>
+    <p>See this register's <a href="http://linked.data.gov.au/org/connegp.html">Conneg P Description page</a> for specifics.</p>
 </div>
 
 <div>
